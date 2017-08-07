463d1702ac69fe973d3001d9e8e374b3e52c5067
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-08-07 01:03:45 -0400

    removed path for comment helper, user env COMMENT_URL To keep the route hidden

--- services/comments/comment-helper.js
+++ services/comments/comment-helper.js
@@ -2,7 +2,8 @@ const fetch = require('isomorphic-fetch');
 function getComments(req,res,next){
     console.log(`hello this is me trying to get the comments`);
     console.log(req.params.id);
-fetch(`http://localhost:3000/search/comments/${req.params.id}`)
+    const commentUrl = process.env.COMMENT_URL;
+fetch(`${commentUrl}${req.params.id}`)
 .then(fetchRes =>{
     // console.log(fetchRes);
     return fetchRes.json()
